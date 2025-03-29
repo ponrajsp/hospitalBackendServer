13590a497792a12ff745e211b5d6399eb0738e64
chetana.lotliker <chetana.lotliker2@gmail.com>
2025-03-29 22:37:08 +0530

    Fix the cors error for the node js api

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,12 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 // User Routes
 app.use('/api', userRoutes);
